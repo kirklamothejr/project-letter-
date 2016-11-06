154d5bc2db2904de79f48931c013234a01469e77
codieme <w.kerri_washington@outlook.com>
2016-11-06 01:30:44 -0600

    Front End Basics

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,render_template
+from flask import Flask, jsonify,render_template, url_for
 import random
 import copy
 import pickle
@@ -37,14 +37,16 @@ def newlevel(gameid):
 @app.route('/game/<gameid>/level/<levelid>/<word>')
 def levelguess(gameid, levelid, word):
     results = {'Winner!!!': False}
+    word = word.lower()
     if word in gstate[gameid]['solutionset'][levelid]:
+        results['Right!'] = True
+        gstate[gameid]['score'] +=1
+        results['score']=gstate[gameid]['score']
         gstate[gameid]['solutionset'][levelid].remove(word)
         if len(gstate[gameid]['solutionset'][levelid]) == 0:
             del gstate[gameid]['solutionset'][levelid]
             if len(gstate[gameid]['solutionset']):
                 results['Winner!!!'] = True
-        results['Right!'] = True
-        gstate[gameid]['score'] +=1
     else:
         results['Right!'] = False
         # TODO: Add possible answers later
