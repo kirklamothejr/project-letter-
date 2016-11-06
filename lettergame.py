149982522fc098ea9b80ad1d956329e16cdbe254
KaenOokami <vsomotun@gmail.com>
2016-11-06 01:34:57 -0700

    SINGLE PLAYER COMPLETE

--- lettergame.py
+++ lettergame.py
@@ -1,26 +1,26 @@
 import requests
 import json
 
+
 def run():
-    r=requests.get('http://127.0.0.1:5000/game/new')
-    game=json.loads(r.text)
+    r = requests.get('http://127.0.0.1:5000/game/new')
+    game = json.loads(r.text)
 
     game_over = False
     while not game_over:
         r = requests.get('http://127.0.0.1:5000/game/{}/level/new'.format(game['id']))
-        level=json.loads(r.text)
+        level = json.loads(r.text)
         guess = input("Give me a word that starts with {}: ".format(level['letter']))
-        r=requests.get('http://127.0.0.1:5000/game/{}/level/{}/{}'.format(game['id'],level['letter'],guess))
-        answer=json.loads(r.text)
+        r = requests.get('http://127.0.0.1:5000/game/{}/level/{}/{}'.format(game['id'], level['letter'], guess))
+        answer = json.loads(r.text)
         if answer['Winner!!!']:
             print("YOU'RE THE BEST")
-            game_over=True
+            game_over = True
         elif answer['Right!']:
             print("NICE! Your Score is {}".format(answer['score']))
         else:
             print("GAME OVER\nYour Score was {}".format(answer['score']))
-            game_over=True
-
+            game_over = True
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import random
 import copy
 import pickle
+import time
 
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ gstate = {}
 def newgame():
     game = {}
     game['id'] = str(random.randint(0, 10000000))
-    game['score']=0
+    game['score'] = 0
     gstate[game['id']] = copy.copy(game)
     gstate[game['id']]['solutionset'] = copy.deepcopy(solutionset)
     return jsonify(game)
@@ -27,25 +28,43 @@ def newlevel(gameid):
     level = {}
     remainings = list(gstate[gameid]['solutionset'].keys())
     level['letter'] = random.choice(remainings)
+    if gstate[gameid]['score'] < 10:
+        level['seconds_to_solve'] = 30
+    elif gstate[gameid]['score'] < 30:
+        level['seconds_to_solve'] = 20
+    elif gstate[gameid]['score'] < 50:
+        level['seconds_to_solve'] = 10
+    else:
+        level['seconds_to_solve'] = 5
+    gstate[gameid]['seconds_to_solve_current_level'] = level['seconds_to_solve']
+    now = int(time.time())
+    level['start_time'] = now
+    gstate[gameid]['last_level_start_time'] = now
     return jsonify(level)
-
 
 @app.route('/game/<gameid>/level/<levelid>/<word>')
 def levelguess(gameid, levelid, word):
+    now = int(time.time())
+    time_cap = gstate[gameid]['last_level_start_time'] + gstate[gameid]['seconds_to_solve_current_level']
     results = {'Winner!!!': False}
-    if word in gstate[gameid]['solutionset'][levelid]:
+
+    if now > time_cap:
+        game_over = True
+        results['Right!'] = False
+        results['score'] = gstate[gameid]['score']
+    elif word in gstate[gameid]['solutionset'][levelid]:
         gstate[gameid]['solutionset'][levelid].remove(word)
         if len(gstate[gameid]['solutionset'][levelid]) == 0:
             del gstate[gameid]['solutionset'][levelid]
             if len(gstate[gameid]['solutionset']):
                 results['Winner!!!'] = True
         results['Right!'] = True
-        gstate[gameid]['score'] +=1
-        results['score']=gstate[gameid]['score']
+        gstate[gameid]['score'] += 1
+        results['score'] = gstate[gameid]['score']
     else:
         results['Right!'] = False
         # TODO: Add possible answers later
-        results['score']=gstate[gameid]['score']
+        results['score'] = gstate[gameid]['score']
     return jsonify(results)
 
 
